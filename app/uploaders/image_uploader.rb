556a65f71d35ba137c35c19b76504467dccc2166
Nícolas Iensen <nicolas@engage.is>
2013-02-08 07:57:51 -0200

    Add resizer to the images

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -29,7 +29,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_fill => [800, 450]
+  process :resize_to_fill => [800, 584]
 
   # def scale(width, height)
   #   # do something
