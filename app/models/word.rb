758ecf10f8ffebd489d7cc4846fd409fc09d3b8e
Nícolas Iensen <nicolas@engage.is>
2013-02-14 12:37:56 -0200

    Change to_param of word

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,4 +2,8 @@ class Word < ActiveRecord::Base
   attr_accessible :long_description, :short_description, :title
   validates :long_description, :short_description, :title, :presence => true
   validates :title, :uniqueness => true
+
+  def to_param
+    "#{self.id}-#{self.title.parameterize}"
+  end
 end
